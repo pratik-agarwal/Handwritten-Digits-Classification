8e2f1866ad43dd3a86b09919405ceb125146cbde
Pratik Agarwal <pagarwal@buffalo.edu>
2019-04-10 21:58:28 -0400

    Facenn script added

--- basecode/facennScript.py
+++ basecode/facennScript.py
@@ -22,16 +22,81 @@ def initializeWeights(n_in,n_out):
     return W
 
 
-
 # Replace this with your sigmoid implementation
 def sigmoid(z):
-    
+    sig = 1.0 / (1.0 + np.exp(-1.0 * z))
+    return sig 
     
 # Replace this with your nnObjFunction implementation
 def nnObjFunction(params, *args):
+    n_input, n_hidden, n_class, training_data, training_label, lambdaval = args
+
+    w1 = params[0:n_hidden * (n_input + 1)].reshape((n_hidden, (n_input + 1)))
+    w2 = params[(n_hidden * (n_input + 1)):].reshape((n_class, (n_hidden + 1)))
+    obj_val = 0
+
+    # Your code here
+    y = np.zeros((training_label.shape[0], n_class))
+    y[np.arange(training_label.shape[0], dtype = "int"), training_label.astype(int)] = 1
+
+    training_data = np.column_stack((np.array(training_data), np.array(np.ones(training_label.shape[0]))))
+
+    K = (sigmoid(np.dot(training_data, w1.T))).shape[0]
+    Z = np.column_stack((sigmoid(np.dot(training_data, w1.T)), np.ones((sigmoid(np.dot(training_data, w1.T))).shape[0])))
+    O = sigmoid(np.dot(Z, w2.T))
+    
+    # deltaL = Ol - Yl
+    error_obtained = O - y
+
+    gradient_w1 = np.dot(((1 - Z) * Z * (np.dot(error_obtained, w2))).T, training_data)
+
+    # Delete 0 type variables
+    delete_var = 0
+
+    gradient_w1 = np.delete(gradient_w1, n_hidden, delete_var)
+
+    gradient_w2 = np.dot(error_obtained.T, Z)
+
+    obj_val = (np.sum(-1 * (y * np.log(O) + (1 - y) * np.log(1 - O)))) / training_data.shape[0] + ((lambdaval / (2 * training_data.shape[0])) * (np.sum(np.square(w1)) + np.sum(np.square(w2))))
+    
+    # Make sure you reshape the gradient matrices to a 1D array. for instance if your gradient matrices are grad_w1 and grad_w2
+    # you would use code similar to the one below to create a flat array
+
+    gradient_w1 = (gradient_w1 + (lambdaval * w1)) / training_data.shape[0]
+    gradient_w2 = (gradient_w2 + (lambdaval * w2)) / training_data.shape[0]
+
+    obj_grad = np.array([])
+    obj_grad = np.concatenate((gradient_w1.flatten(), gradient_w2.flatten()), 0)
+
+    # print(obj_val)
+    # print(obj_grad)
+
+    return (obj_val, obj_grad)
+
     
 # Replace this with your nnPredict implementation
 def nnPredict(w1,w2,data):
+    # Your code here
+    N, D = data.shape
+    
+    # Bias
+    B = np.ones(N)*1
+
+    # Concatenating Bias
+    data = np.column_stack((data, B))
+
+    # Output at the hidden layer
+    z = sigmoid(np.dot(data, w1.T))
+
+    # Output at the output layer
+    o = sigmoid(np.dot(np.column_stack((z, B.T)), w2.T))
+
+    labels = o.argmax(axis = 1)
+
+    # print(labels.shape)
+
+    return labels
+    
 
 # Do not change this
 def preprocess():
